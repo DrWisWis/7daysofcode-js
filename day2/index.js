1f2444429492e55dd0c3aec51039aa9ae9cf5796
Igor Ibiapino <ibiapinoigor@gmail.com>
2025-08-17 23:58:58 -0300

    terminado o execercio

--- day2/index.js
+++ day2/index.js
@@ -2,4 +2,4 @@ const name = prompt("Qual o seu nome?")
 const age = prompt("Quantos anos vc tem?")
 const ling_prop = prompt("Qual linguagem de programação vc esta estudanto?")
 
-console.log("")
+console.log(`Olá ${name}, você tem ${age} anos e já está aprendendo ${ling_prop}  `)
